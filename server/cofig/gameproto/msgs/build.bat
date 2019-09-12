479bc9c4d95fc32268b8d750e028da8fa48daf9c
magicsea <magicsea_wy@163.com>
2019-09-12 16:31:41 +0800

    proto vendor config

--- server/cofig/gameproto/msgs/build.bat
+++ server/cofig/gameproto/msgs/build.bat
@@ -1,1 +1,1 @@
-protoc -I=. -I=.\..\..\..\src  -I=%GOPATH%\src -I=..\.. --gogoslick_out=plugins=grpc:.\..\..\..\src\gameproto\msgs\ *.proto 
+protoc -I=. -I=.\..\..\..\src -I=.\..\..\..\src\vendor  -I=%GOPATH%\src -I=..\.. --gogoslick_out=plugins=grpc:.\..\..\..\src\gameproto\msgs\ *.proto 
